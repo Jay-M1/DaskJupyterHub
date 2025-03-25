bb5caa280bbf2d9860e550e57e2911da0c09bccb
Jasin <uhsur@student.kit.edu>
2025-03-25 14:26:49 +0100

    notebook connects automatically to gateway server

--- jupy/notebook/run_notebook.sh
+++ jupy/notebook/run_notebook.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-# Execute the notebook using nbconvert.
-echo "Running the notebook..."
-jupyter nbconvert --to notebook --execute /home/jovyan/work/gateway.ipynb --output /home/jovyan/your_notebook_executed.ipynb
+set -e
 
-# Start the Jupyter Notebook server
+echo "Running the notebook..."
+jupyter nbconvert --to notebook \
+  --execute /home/jovyan/work/gateway.ipynb \
+  --output /home/jovyan/work/your_notebook_executed.ipynb \
+  --ExecutePreprocessor.allow_errors=False \
+  --log-level=INFO
+
+echo "Notebook execution complete."
+
 echo "Starting the Jupyter Notebook server..."
 exec start-notebook.sh
