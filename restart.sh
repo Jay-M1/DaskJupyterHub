57d2c6e5dee8af51dce837cdb3e39f90fe85c895
Jasin <uhsur@student.kit.edu>
2025-03-25 12:21:29 +0100

    working setup

--- restart.sh
+++ restart.sh
@@ -5,3 +5,5 @@ docker network rm $(docker network ls -q)
 
 docker compose build
 docker compose up -d
+
+# docker exec -it my_jupyterhub-htrocky-1 /bin/bash
---
+++ rocky/dask-gateway_server_config.py
@@ -0,0 +1,17 @@
+# Configure the gateway to use PBS
+c.DaskGateway.backend_class = (
+    "dask_gateway_htcondor.htcondor.HTCondorBackend"
+)
+
+# The resource limits for a worker
+c.HTCondorClusterConfig.worker_memory = '16 G'
+c.HTCondorClusterConfig.worker_cores = 8
+
+# Increase startup timeouts to 5 min (600 seconds) each
+c.HTCondorBackend.cluster_start_timeout = 600
+c.HTCondorBackend.worker_start_timeout = 600
+
+c.HTCondorClusterConfig.docker_image = "giffels/coffea-dask-cc7-gateway"
+c.HTCondorClusterConfig.extra_jdl = {"accounting_group": "dask"}
+c.HTCondorClusterConfig.staging_directory = "/tmp/dask/.dask-gateway/"
+c.HTCondorClusterConfig.tls_worker_node_prefix_path = ""
