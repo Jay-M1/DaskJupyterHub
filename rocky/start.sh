80e29233035f26d09866713fea95e2b8e629c628
Jasin <uhsur@student.kit.edu>
2025-04-04 19:46:52 +0200

    cluster is working

--- rocky/start.sh
+++ rocky/start.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+echo ">>> in start.sh"
+
 condor_master
-dask-gateway-server --config dask-gateway_server_config.py
+
+echo ">>> executed condor_master"
+
+dask-gateway-server
+
+echo ">>> Executed dask-gateway-server --config /work/dask-gateway_server_config.py"
