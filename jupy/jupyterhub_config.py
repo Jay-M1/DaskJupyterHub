8b11343862368f779c27b36716e2d33848c2a1c4
Jay-M1 <uhsur@student.kit.edu>
2025-06-10 14:39:02 +0200

    Jupyter spawner spawns notebook with user uid but without home dir

--- jupy/jupyterhub_config.py
+++ jupy/jupyterhub_config.py
@@ -1,4 +1,6 @@
 import pwd
+import os
+import warnings
 
 c = get_config() # type: ignore[name-defined]
 
@@ -15,6 +17,9 @@ c.LDAPAuthenticator.tls_strategy = "on_connect"
 c.LDAPAuthenticator.lookup_dn = False
 c.LDAPAuthenticator.user_search_base ="dc=ekp,dc=physik,dc=uni-karlsruhe,dc=de"
 c.LDAPAuthenticator.bind_dn_template = ["uid={username},ou=people,dc=ekp,dc=physik,dc=uni-karlsruhe,dc=de"]
+c.LDAPAuthenticator.enable_auth_state = True
+c.LDAPAuthenticator.auth_state_attributes = ["uid", "uidNumber", "gidNumber"]
+c.CryptKeeper.keys = [os.urandom(32)]
 
 # Allow all users to log in
 c.Authenticator.allow_all = True
@@ -24,7 +29,7 @@ c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 
 # Set the Docker image for single-user servers
 c.DockerSpawner.image = 'jnotebook_image'
-c.DockerSpawner.notebook_dir = '/home/{username}/work'
+#c.DockerSpawner.notebook_dir = '/home/{username}/work'
 
 # Remove containers once they are stopped
 c.DockerSpawner.remove = False
@@ -32,15 +37,14 @@ c.DockerSpawner.use_internal_ip = True
 c.DockerSpawner.network_name = 'my_jupyterhub_jupyternet'
 c.Spawner.debug = True
 #c.DockerSpawner.debug = True
-#c.DockerSpawner.cmd = ['jupyterhub-singleuser']
+#c.DockerSpawner.cmd = ["start-notebook.sh"]
 
 # Define the environment variables for the user
-def define_environment(spawner):
-    user_name = spawner.user.name
-    user_id = pwd.getpwnam(user_name).pw_uid
-    group_id = pwd.getpwnam(user_name).pw_gid
-    spawner.environment.update(dict(NB_USER=user_name,
-                                    NB_UID=str(user_id),
-                                    NB_GID=str(group_id)))
+async def define_environment(spawner):
+    auth_state = await spawner.user.get_auth_state()
+    warnings.warn(f"---------------------> {auth_state}")
+    spawner.environment["NB_UID"] = str(auth_state["user_attributes"]["uidNumber"][0])
+    spawner.environment["NB_GID"] = str(auth_state["user_attributes"]["gidNumber"][0])
+    spawner.environment["NB_USER"] = str(auth_state["user_attributes"]["uid"][0])
 
 c.Spawner.pre_spawn_hook = define_environment
