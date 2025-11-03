94aa1f285492c708e8240a8ce84e307090e1e847
Jasin Mustafi <jasin.mustafi@student.kit.edu>
2025-11-03 09:41:16 +0100

    added central requiremtns file

--- restart.sh
+++ restart.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+
+for dir in rocky coffea-backup jupy/notebook; do
+    cp requirements.txt $dir/requirements.txt
+done
+
 docker network prune -f
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
